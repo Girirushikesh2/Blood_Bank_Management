996874ea70fe0c22c45ce966dcf1e60658f3c520
Rushikesh <girirushikesh2@gmail.com>
2022-01-13 12:43:50 +0530

    updated 2 commit

--- Bloodbankmanagement/src/app/admin-stock/admin-stock.component.ts
+++ Bloodbankmanagement/src/app/admin-stock/admin-stock.component.ts
@@ -9,22 +9,49 @@ import { StockModel } from './stock.model';
   styleUrls: ['./admin-stock.component.css']
 })
 export class AdminStockComponent implements OnInit {
-
+ 
   StockModelObj : StockModel = new StockModel();
 
   stockform = new FormGroup({
     
   })
 
+  get date() {
+    return this.stockform.get('date'); 
+  }
 
-get bloodgroup() {
-  return this.stockform.get('bloodgroup'); 
-}
+  get Ap() {
+    return this.stockform.get('Ap'); 
+  }
 
-get bagquantity() {
-  return this.stockform.get('bagquantity'); 
-}
+  get An() {
+    return this.stockform.get('An'); 
+  }
 
+  get Bp() {
+    return this.stockform.get('Bp'); 
+  }
+
+  get Bn() {
+    return this.stockform.get('Bn'); 
+  }
+
+  get Op() {
+    return this.stockform.get('Op'); 
+  }
+
+  get On() {
+    return this.stockform.get('On'); 
+  }
+
+  get ABp() {
+    return this.stockform.get('ABp'); 
+  }
+  
+  get ABn() {
+    return this.stockform.get('ABn'); 
+  }
+  
 
 
 
@@ -39,19 +66,34 @@ get bagquantity() {
 
   ngOnInit(): void {
     this.stockform = this.formbuilder.group({
-      bloodgroup : new FormControl("",[Validators.required]),
-      bagquantity :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      Ap :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      An :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      Bp :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      Bn :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      Op :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      On :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      ABp :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      ABn :new FormControl("",[ Validators.required, Validators.pattern("^[0-9]{2}$")]),
+      date :new FormControl("",[ Validators.required]),
+      
             
     })
   }
 
   postCustomerDetails(){
-    this.StockModelObj.bloodgroup = this.stockform.value.bloodgroup;
-    this.StockModelObj.bagquantity = this.stockform.value.bagquantity;
-
+    this.StockModelObj.Ap = this.stockform.value.Ap;
+    this.StockModelObj.An = this.stockform.value.An;
+    this.StockModelObj.Bp = this.stockform.value.Bp;
+    this.StockModelObj.Bn = this.stockform.value.Bn;
+    this.StockModelObj.Op = this.stockform.value.Op;
+    this.StockModelObj.On = this.stockform.value.On;
+    this.StockModelObj.ABp = this.stockform.value.ABp;
+    this.StockModelObj.ABn = this.stockform.value.ABn;
+    this.StockModelObj.date = this.stockform.value.date;
+ 
  
 
-    this.stock.postCust(this.StockModelObj)
+    this.stock.postSt(this.StockModelObj)
       .subscribe(res=>{
       console.log(res);
       alert("Stock Added Successfully")
@@ -62,6 +104,42 @@ get bagquantity() {
     },
     err=>{
       alert("Something went wrong")
+    })
+  }
+  onEdit(row:any){
+    
+    this.StockModelObj.id = row.id;
+    this.stockform.controls['Ap'].setValue(row.Ap);
+    this.stockform.controls['An'].setValue(row.An);
+    this.stockform.controls['Bp'].setValue(row.Bp);
+    this.stockform.controls['Bn'].setValue(row.Bn);
+    this.stockform.controls['Op'].setValue(row.Op);
+    this.stockform.controls['On'].setValue(row.Pn);
+    this.stockform.controls['ABp'].setValue(row.ABp);
+    this.stockform.controls['ABn'].setValue(row.ABn);
+    this.stockform.controls['date'].setValue(row.date);
+    
+ 
+    
+  }
+
+  updateStockDetails(){
+    this.StockModelObj.Ap = this.stockform.value.Ap;
+    this.StockModelObj.An = this.stockform.value.An;
+    this.StockModelObj.Bp = this.stockform.value.Bp;
+    this.StockModelObj.Bn = this.stockform.value.Bn;
+    this.StockModelObj.Op = this.stockform.value.Op;
+    this.StockModelObj.On = this.stockform.value.On;
+    this.StockModelObj.ABp = this.stockform.value.ABp;
+    this.StockModelObj.ABn = this.stockform.value.ABn;
+        
+    this.stock.updateStock(this.StockModelObj, this.StockModelObj.id)
+    .subscribe(res=>{
+      alert("Updated successfully");
+      let ref = document.getElementById('cancel')
+      ref?.click();
+      this.stockform.reset();
+      
     })
   }
 
